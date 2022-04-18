956a652529f63f2ce3becb8f5f80e6e5a6d96d07
Riquelme <riquelmebandeira1@outlook.com>
2022-04-18 16:55:02 -0300

    feat: creates the Vehicle schema

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const VehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type Vehicle = z.infer<typeof VehicleSchema>;
