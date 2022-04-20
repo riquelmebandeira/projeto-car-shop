10be979168601a870c4209ce7f6dfd31fccd3ff8
Riquelme <riquelmebandeira1@outlook.com>
2022-04-20 09:11:52 -0300

    feat: creates the CarModel class

---
+++ src/models/CarModel.ts
@@ -0,0 +1,23 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Car } from '../interfaces/CarInterface';
+import MongoModel from '.';
+
+interface CarDocument extends Car, Document {}
+
+const carSchema = new Schema<CarDocument>({
+  status: Boolean,
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class CarModel extends MongoModel<Car> {
+  constructor(model = createModel('Cars', carSchema)) {
+    super(model);
+  }
+}
+
+export default CarModel;
