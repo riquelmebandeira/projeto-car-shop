831350573543cb3e0ca39ead8786e9d54dc8a770
Riquelme <riquelmebandeira1@outlook.com>
2022-04-20 10:26:47 -0300

    feat: creates the Controller class

---
+++ src/controllers/index.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import ControllerErrors from '../enums/ControllerErros';
+import Service from '../services';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+abstract class Controller<T> {
+  private _route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: Service<T>, route: string) {
+    this._route = route;
+  }
+
+  get route() { return this._route; }
+
+  create = async (
+    req: Request,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const obj = await this.service.create(req.body);
+  
+      if (!obj) return res.status(404).json({ error: this.errors.notFound });
+
+      if ('error' in obj) return res.status(400).json(obj);
+      
+      return res.status(201).json(obj);
+    } catch (err) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+}
+
+export default Controller;
---
+++ src/enums/ControllerErros.ts
@@ -0,0 +1,8 @@
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  invalidId = 'Id must have 24 hexadecimal characters',
+  badRequest = 'Bad request',
+}
+
+export default ControllerErrors;
