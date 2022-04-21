04143cbbb3c3455458a2dd331ae98468c6582f98
Riquelme <riquelmebandeira1@outlook.com>
2022-04-21 13:37:45 -0300

    refactor: creates a directory for the mocks and change the test text

---
+++ src/tests/unit/mocks/CarsMocks.ts
@@ -0,0 +1,24 @@
+export const validCar = {
+  'model': 'Amarok',
+  'year': 2022,
+  'color': 'Preto',
+  'buyValue': 250.000,
+  'doorsQty': 4,
+  'seatsQty': 5
+}
+
+export const invalidCar = {
+  'model': 'Amarok',
+  'year': 2022,
+  'color': 'Preto',
+}
+
+export const createdCar = {
+  'id': 'dc8b1dbb6a8ab33712ccec68',
+  'model': 'Amarok',
+  'year': 2022,
+  'color': 'Preto',
+  'buyValue': 250.000,
+  'doorsQty': 4,
+  'seatsQty': 5
+}
--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -2,38 +2,20 @@ import * as sinon from 'sinon';
 import { expect } from 'chai';
 import CarModel from '../../../models/CarModel';
 import { Model } from 'mongoose';
+import { createdCar, validCar } from '../mocks/CarsMocks';
 
 const carModel = new CarModel()
 
-const newCar = {
-    'model': 'Amarok',
-    'year': 2022,
-    'color': 'Preto',
-    'buyValue': 250.000,
-    'doorsQty': 4,
-    'seatsQty': 5
-  }
+describe('Ao chamar, no model de Car', () => {
 
-const createdCar = {
-  'id': 'dc8b1dbb6a8ab33712ccec68',
-  'model': 'Amarok',
-  'year': 2022,
-  'color': 'Preto',
-  'buyValue': 250.000,
-  'doorsQty': 4,
-  'seatsQty': 5
-}
-
-describe('Tesde de Model', () => {
-
-  describe('Car', async () => {
+  describe('o método create', async () => {
 
     before(() => sinon.stub(Model, 'create').resolves(createdCar))
 
     after(() => (Model.create as sinon.SinonStub).restore())
 
     it('Cria um novo carro com sucesso', async () => {
-      expect(await carModel.create(newCar)).to.be.equal(createdCar);
+      expect(await carModel.create(validCar)).to.be.equal(createdCar);
     });
   })
 });
