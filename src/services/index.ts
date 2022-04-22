6a2bd651aabfe33fecf4c05b10b07f648148a028
Riquelme <riquelmebandeira1@outlook.com>
2022-04-22 10:22:04 -0300

    feat: adds id validation for the readOne method

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,8 @@
-import { ZodAny } from 'zod';
+import { z, ZodAny } from 'zod';
 import { Model } from '../interfaces/ModelInterface';
 import ServiceError from '../interfaces/ServiceErrorInterface';
 
+const idSchema = z.string().length(24).regex(/[a-z][0-9]+/);
 abstract class Service<T> {
   constructor(public model: Model<T>, public documentSchema: ZodAny) { }
 
@@ -17,7 +18,13 @@ abstract class Service<T> {
 
   public async read(): Promise<T[]> { return this.model.read(); }
 
-  public async readOne(id: string): Promise<T | null> {
+  public async readOne(id: string): Promise<T | null | ServiceError> {
+    const parsed = idSchema.safeParse(id);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+
     return this.model.readOne(id); 
   }
 }
