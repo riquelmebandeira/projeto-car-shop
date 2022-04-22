827b1042d9cf170ac1acbe3d98dde16ef5656122
Riquelme <riquelmebandeira1@outlook.com>
2022-04-22 14:24:22 -0300

    test: adds coverage to carModel's readOne method

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -9,9 +9,7 @@ const carModel = new CarModel()
 describe('Ao chamar, no model de Car', () => {
 
   describe('o método create', async () => {
-
     before(() => sinon.stub(Model, 'create').resolves(createdCar))
-
     after(() => (Model.create as sinon.SinonStub).restore())
 
     it('Cria um novo carro com sucesso', async () => {
@@ -20,13 +18,34 @@ describe('Ao chamar, no model de Car', () => {
   })
 
   describe('o método read', async () => {
-
     before(() => sinon.stub(Model, 'find').resolves(allCars))
-
     after(() => (Model.find as sinon.SinonStub).restore())
 
     it('Retorna todos os carros', async () => {
       expect(await carModel.read()).to.be.equal(allCars);
     });
+  })
+
+  describe('o método readOne', async () => {
+    before(() => sinon.stub(Model, 'findOne')
+    .onCall(0)
+    .resolves(null)
+    .onCall(1)
+    .resolves(createdCar)
+    )
+
+    after(() => (Model.findOne as sinon.SinonStub).restore())
+
+    describe('passando um id inexistente', () => {
+      it('É retornado null', async () => {
+        expect(await carModel.readOne('invalidId')).to.be.equal(null);
+      })
+    })
+
+    describe('passando um id existente', () => {
+      it('É retornado o objeto do carro', async () => {
+        expect(await carModel.readOne('validId')).to.be.equal(createdCar);
+      })
+    })
   })
 });
