307990b59cb5047dd0ca0b085547dce03c3d5c0e
Riquelme <riquelmebandeira1@outlook.com>
2022-04-22 17:32:37 -0300

    test: adds coverage to CarController's readOne method

--- src/tests/unit/controllers/CarController.test.ts
+++ src/tests/unit/controllers/CarController.test.ts
@@ -4,11 +4,13 @@ import CarController from '../../../controllers/CarController';
 import { allCars, createdCar, invalidCar, validCar } from '../mocks/CarsMocks';
 import ServiceError from '../../../interfaces/ServiceErrorInterface';
 import { Response } from 'express';
+import ControllerErrors from '../../../enums/ControllerErros';
 
 const carController = new CarController()
 
-const mockRequest = (body: any) => ({
+const mockRequest = (body?: any, params?: any) => ({
   body,
+  params,
 });
 
 const mockResponse = () => {
@@ -76,6 +78,62 @@ describe('Ao chamar, no controller de Car', () => {
     
     it('É chamado o status com o código 200', () => {
       expect(res.status.calledWith(200)).to.be.equal(true)
+    })
+  })
+
+  describe('o método readOne', async () => {
+    before(() => (
+      sinon.stub(carController.service, 'readOne')
+      .onCall(0)
+      .resolves({error: {}} as ServiceError)
+      .onCall(1)
+      .resolves(null)
+      .onCall(2)
+      .resolves(createdCar)
+      ))
+
+    after(() => (carController.service.readOne as sinon.SinonStub).restore())
+
+    describe('passando um id inválido', async () => {
+      const req = mockRequest({}, { id: 'invalidId' }) as any;
+      const res = mockResponse() as any;
+
+      it('É chamado o json com um erro', async () => {
+        await carController.readOne(req, res);
+        expect(res.json.calledWith({ error: ControllerErrors.invalidId })).to.be.equal(true);
+      });
+      
+      it('É chamado o status com o código 400', () => {
+        expect(res.status.calledWith(400)).to.be.equal(true)
+      })
+    })
+
+    describe('passando um id inexistente', async () => {
+      const req = mockRequest({}, { id: 'unexistingId' }) as any;
+      const res = mockResponse() as any;
+
+      it('É chamado o json com um erro', async () => {
+        await carController.readOne(req, res);
+        expect(res.json.calledWith({ error: ControllerErrors.notFound })).to.be.equal(true);
+      });
+      
+      it('É chamado o status com o código 404', () => {
+        expect(res.status.calledWith(404)).to.be.equal(true)
+      })
+    })
+
+    describe('passando um id válido', () => {
+      const req = mockRequest({}, { id: 'validId' }) as any;
+      const res = mockResponse() as any;
+  
+      it('É chamado o json com o objeto do veículo', async () => {
+        await carController.readOne(req, res);
+        expect(res.json.calledWith(createdCar)).to.be.equal(true);
+      });
+
+      it('É chamado o status com o código 200', () => {
+        expect(res.status.calledWith(200)).to.be.equal(true)
+      })
     })
   })
 });
