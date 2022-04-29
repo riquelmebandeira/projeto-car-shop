ea0d5f43528a31c6973f911886da124eb35baaf6
Riquelme <riquelmebandeira1@outlook.com>
2022-04-29 13:18:34 -0300

    test: adds coverage to the car controller's update method

--- src/tests/unit/controllers/CarController.test.ts
+++ src/tests/unit/controllers/CarController.test.ts
@@ -21,7 +21,7 @@ const mockResponse = () => {
 };
 
 describe('Ao chamar, no controller de Car', () => {
-  describe('o método create', async () => {
+  describe('O método create', async () => {
     before(() => (
       sinon.stub(carController.service, 'create')
       .onCall(0)
@@ -63,7 +63,7 @@ describe('Ao chamar, no controller de Car', () => {
     })
   })
 
-  describe('o método read', async () => {
+  describe('O método read', async () => {
     before(() => (sinon.stub(carController.service, 'read').resolves(allCars)));
     after(() => (carController.service.read as sinon.SinonStub).restore());
 
@@ -81,7 +81,7 @@ describe('Ao chamar, no controller de Car', () => {
     })
   })
 
-  describe('o método readOne', async () => {
+  describe('O método readOne', async () => {
     before(() => (
       sinon.stub(carController.service, 'readOne')
       .onCall(0)
@@ -128,6 +128,78 @@ describe('Ao chamar, no controller de Car', () => {
   
       it('É chamado o json com o objeto do veículo', async () => {
         await carController.readOne(req, res);
+        expect(res.json.calledWith(createdCar)).to.be.equal(true);
+      });
+
+      it('É chamado o status com o código 200', () => {
+        expect(res.status.calledWith(200)).to.be.equal(true)
+      })
+    })
+  })
+
+  describe('O método update', async () => {
+    before(() => (
+      sinon.stub(carController.service, 'update')
+      .onCall(0)
+      .resolves({ error: ControllerErrors.invalidId } as unknown as ServiceError)
+      .onCall(1)
+      .resolves({ error: {} } as ServiceError)
+      .onCall(2)
+      .resolves(null)
+      .onCall(3)
+      .resolves(createdCar)
+      ))
+
+    after(() => (carController.service.update as sinon.SinonStub).restore())
+
+    describe('passando um id inválido', async () => {
+      const req = mockRequest({}, { id: 'invalidId' }) as any;
+      const res = mockResponse() as any;
+
+      it('É chamado o json com um erro', async () => {
+        await carController.update(req, res);
+        expect(res.json.calledWith({ error: ControllerErrors.invalidId })).to.be.equal(true);
+      });
+      
+      it('É chamado o status com o código 400', () => {
+        expect(res.status.calledWith(400)).to.be.equal(true)
+      })
+    })
+
+    describe('passando um body inválido', async () => {
+      const req = mockRequest({}, { id: 'validId' }) as any;
+      const res = mockResponse() as any;
+
+      it('É chamado o json com um erro', async () => {
+        await carController.update(req, res);
+        expect(res.json.calledWith({ error: {} })).to.be.equal(true);
+      });
+      
+      it('É chamado o status com o código 400', () => {
+        expect(res.status.calledWith(400)).to.be.equal(true)
+      })
+    })
+
+    describe('passando um id inexistente', async () => {
+      const req = mockRequest({}, { id: 'unexistingId' }) as any;
+      const res = mockResponse() as any;
+
+      it('É chamado o json com um erro', async () => {
+        await carController.update(req, res);
+        expect(res.json.calledWith({ error: ControllerErrors.notFound })).to.be.equal(true);
+      });
+      
+      it('É chamado o status com o código 404', () => {
+        expect(res.status.calledWith(404)).to.be.equal(true)
+      })
+    })
+
+    describe('passando dados válidos', () => {
+      const req = mockRequest({}, { id: 'validId' }) as any;
+      const res = mockResponse() as any;
+  
+      it('É chamado o json com o objeto do veículo', async () => {
+        await carController.update(req, res);
         expect(res.json.calledWith(createdCar)).to.be.equal(true);
       });
 
