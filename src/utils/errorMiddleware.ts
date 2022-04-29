d80896a417fa7020dfb5788f78d7a276b90b3090
Riquelme <riquelmebandeira1@outlook.com>
2022-04-29 19:10:35 -0300

    feat: creates a enum of status codes

--- src/utils/errorMiddleware.ts
+++ src/utils/errorMiddleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, ErrorRequestHandler, NextFunction } from 'express';
 import { ZodError } from 'zod';
 import CustomError from './CustomError';
 import ErrorMessages from './ErrorMessages';
+import StatusCode from './StatusCode';
 
 const errorMiddleware = ( 
   err: ErrorRequestHandler, 
@@ -10,14 +11,16 @@ const errorMiddleware = (
   next: NextFunction,
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json(err);
+    return res.status(StatusCode.BAD_REQUEST).json(err);
   }
 
   if (err instanceof CustomError) {
     return res.status(err.status).json({ error: err.message });
   }
 
-  return res.status(500).json({ error: ErrorMessages.INTERNAL_SERVER_ERROR });
+  return res.status(StatusCode.INTERNAL_SERVER_ERROR).json(
+    { error: ErrorMessages.INTERNAL_SERVER_ERROR },
+  );
 
   next();
 };
